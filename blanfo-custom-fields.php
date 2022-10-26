56116265d23d6312038ff295c72638c5fbf5586c
Zein <zein.z@realtyna.net>
2022-10-26 14:03:06 +0330

    Initial Commit

---
+++ blanfo-custom-fields.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name:       Blanfo Custom Fields
+ * Plugin URI:        https://blanfo.ir
+ * Description:       Custom Fields on Woocommerce
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Ali Zeinodini
+ * Author URI:        https://alizeinodini.ir
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       bcf
+ * Domain Path:       /languages
+ */
